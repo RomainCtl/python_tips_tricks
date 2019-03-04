38a9f77b883936fd146302bc89b6a82c2937cc28
Chantrel Romain <39402602+RomainCtl@users.noreply.github.com>
2019-03-04 17:14:14 +0100

    Create dbconnect.py

---
+++ sqlalchemy/dbconnect.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from functools import wraps
+// from config import Config
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@host:port/schema'
+    MYSQL_DATABASE_CHARSET = 'utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 280
+    SQLALCHEMY_POOL_TIMEOUT = 20
+
+Session = scoped_session(sessionmaker(bind=create_engine(Config.SQLALCHEMY_DATABASE_URI), autoflush=True))
+
+def dbconnect(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        current_session = Session()
+        try:
+            res = func(*args, session=current_session, **kwargs)
+            current_session.commit()
+        except Exception as e:
+            current_session.rollback()
+            if len(e.args) > 0: res = dict(message=type(e).__name__ +": "+e.args[0]), e.args[1] if len(e.args) > 2 else 500
+            else: res = dict(message=type(e).__name__ +": "+str(e)), 500
+        finally:
+            current_session.close()
+            Session.remove()
+            return res
+    return inner
